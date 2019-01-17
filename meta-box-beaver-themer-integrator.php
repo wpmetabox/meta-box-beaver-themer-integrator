d43d24862b1cd8eb92abdffad5b1486928041906
Anh Tran <anhtnt@elightup.com>
2019-01-17 15:50:00 +0700

    Version 1.3.0

--- meta-box-beaver-themer-integrator.php
+++ meta-box-beaver-themer-integrator.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Plugin Name: Meta Box - Beaver Themer Integrator
- * Plugin URI: https://metabox.io/plugins/meta-box-beaver-themer-integrator/
- * Description:Integrates Meta Box and Beaver Themer
- * Author: MetaBox.io
- * Author URI: https://metabox.io
- * Text Domain:meta-box-beaver-themer-integrator
- * Domain Path:/languages
- * Version: 1.2.1
+ * Plugin URI:  https://metabox.io/plugins/meta-box-beaver-themer-integrator/
+ * Description: Integrates Meta Box and Beaver Themer.
+ * Author:      MetaBox.io
+ * Author URI:  https://metabox.io
+ * Text Domain: meta-box-beaver-themer-integrator
+ * Domain Path: /languages
+ * Version:     1.3.0
  *
  * @package    Meta Box
  * @subpackage Meta Box Beaver Themer Integrator
